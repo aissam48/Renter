09f8471a42e98140b94a7bc9892dde4f257ed1f8
Aissam Elboudi <issam.rma10@gmail.com>
2021-12-27 13:53:06 +0100

    first steps

--- app/src/main/java/com/renter/android/Ui/SignIN.kt
+++ app/src/main/java/com/renter/android/Ui/SignIN.kt
@@ -8,5 +8,10 @@ class SignIN : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+
+
+        //saluuuuuuuuuuuuuuuut
+
     }
 }
